ec4e1776594ba6b1a5585e3d0b7d8a056817a447
achinthapalli <achinthapalli@nextsphere.com>
2020-02-19 15:55:27 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './layout/about-us/about-us.component';
+import { ContactUsComponent } from './layout/contact-us/contact-us.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'contact-us', component: ContactUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { LandingComponent } from './layout/landing/landing.component';
+import { AboutUsComponent } from './layout/about-us/about-us.component';
+import { ContactUsComponent } from './layout/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LandingComponent
+    LandingComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
